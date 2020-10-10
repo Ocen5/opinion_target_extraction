6ba3d6fc6c4c6023e86901d60375f7089c388c17
Ocen5 <45494956+Ocen5@users.noreply.github.com>
2020-10-10 10:53:10 +0200

    implementazione R1 #3
    Refactor del modulo aggiunto nell'issue 2.
    Aggiunta implementazione della Regola1.
    stanford-core-nlp-4.0.0 : https://drive.google.com/drive/folders/1PSFRFV9WtnfMlkAOselw5qb_yuqixTe7?usp=sharing

--- StanfordParser/SCnlp.py
+++ StanfordParser/SCnlp.py
@@ -1,24 +1,66 @@
 from stanfordcorenlp import StanfordCoreNLP
 import logging
 import sys
+import numpy as np
 
 
 
-def provaf(local_corenlp_path, input):
+def SC(local_corenlp_path, input):
   nlp = StanfordCoreNLP(local_corenlp_path, quiet=False, logging_level=logging.DEBUG)
 
-  sys.stdout = open("output.txt", "w")
 
-  print('Tokenize:', nlp.word_tokenize(input))
+  Tokenize = nlp.word_tokenize(input)
+  POS = nlp.pos_tag(input)
+  Dependency = nlp.dependency_parse(input)
+
+  print("Tokeninze: ", Tokenize)
   print('\n')
-  print('Part of Speech:', nlp.pos_tag(input))
+  print("Part Of Speech: ", POS)
   print('\n')
-  print('Named Entities:', nlp.ner(input))
-  print('\n')
-  print('Constituency Parsing:', nlp.parse(input))
-  print('\n')
-  print('Dependency Parsing:', nlp.dependency_parse(input))
+  print("Dependency Parse: ", Dependency)
   print('\n')
 
   nlp.close()
-  sys.stdout.close()
+  return Tokenize, POS, Dependency
+
+
+def R1(local_corenlp_path, input):
+
+  nlp = StanfordCoreNLP(local_corenlp_path, quiet=False, logging_level=logging.DEBUG)
+
+  toke, pos, depe = SC(local_corenlp_path, input)
+
+  tok = np.array(toke)
+  list_target = []
+
+  Op = 'opinion-lexicon-English/positive-words.txt'
+  On = 'opinion-lexicon-English/negative-words.txt'
+  val = 'JJ'
+
+  for item in pos:
+    if item[1] == val:
+      agg = item[0]
+
+      with open(Op) as myfile:
+        if agg in myfile.read():
+          label = 'positive'
+      with open(On) as myfile:
+        if agg in myfile.read():
+          label = 'negative'
+
+          MR = ['amod', 'advmod', 'rcmod']
+          for items in depe:
+            if items[0] in MR:
+              opinion = items[2]
+              target = items[1]
+              o = tok[opinion-1]
+              t = tok[target-1]
+              list_target.append(t)
+
+  return list_target
+
+
+
+
+
+
--- StanfordParser/main.py
+++ StanfordParser/main.py
@@ -1,12 +1,28 @@
 import SCnlp
+import sys
 
 local_corenlp_path = 'stanford-corenlp-4.0.0/stanford-corenlp-4.0.0'
 
+with open('input.txt', 'r') as infile, \
+        open('input_processato.txt', 'w') as outfile:
+  data = infile.read()
+  data = data.replace(".", "")
+  data = data.replace(",", "")
+  outfile.write(data)
 
-text = open('input.txt', 'r')
+
+text = open('input_processato.txt', 'r')
 lines = text.readlines()
 input = ''
 for i in lines:
   input = input + i
 
-SCnlp.provaf(local_corenlp_path, input)
+
+Tokenize, POS, Dependency = SCnlp.SC(local_corenlp_path, input)
+
+sys.stdout = open("output.txt", "w")
+
+target = SCnlp.R1(local_corenlp_path, input)
+print("lista dei potenziali target: ", set(target))
+
+sys.stdout.close()

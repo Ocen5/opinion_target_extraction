1ee81f0aa9e10d491ed93e10bf4311a0dc4c2065
Ocen5 <45494956+Ocen5@users.noreply.github.com>
2020-10-20 10:15:29 +0200

    refactor generale- implementazione regola3 #5

--- StanfordParser/SCnlp.py
+++ StanfordParser/SCnlp.py
@@ -2,6 +2,8 @@ from stanfordcorenlp import StanfordCoreNLP
 import logging
 import sys
 import numpy as np
+import nltk
+from nltk.tokenize import sent_tokenize
 
 
 
@@ -27,7 +29,9 @@ def SC(local_corenlp_path, input):
 def R1(local_corenlp_path, input):
 
 
+
   toke, pos, depe = SC(local_corenlp_path, input)
+
 
   print("Tokeninze: ", toke)
   print('\n')
@@ -78,14 +82,19 @@ def R1(local_corenlp_path, input):
   return set(list_target)
 
 
-def R2(local_corenlp_path, input):
+def R2(local_corenlp_path, input, file_target):
 
-  list_target = R1(local_corenlp_path, input)
+  #list_target = R1(local_corenlp_path, input)
 
   list_opinion = []
   toke, pos, depe = SC(local_corenlp_path, input)
 
-
+  print("Tokeninze: ", toke)
+  print('\n')
+  print("Part Of Speech: ", pos)
+  print('\n')
+  print("Dependency Parse: ", depe)
+  print('\n')
 
   tok = np.array(toke)
   val = ['NN', 'NNS']
@@ -94,31 +103,56 @@ def R2(local_corenlp_path, input):
     if item[1] in val:
       sost = item[0]
 
-      if sost in list_target:
-        MR = ['amod', 'advmod', 'rcmod']
-        for items in depe:
-          if items[0] in MR:
-            opinion = items[2]
-            target = items[1]
-            if pos[opinion - 1].__contains__('JJ') or pos[opinion - 1].__contains__('JJS') \
-                    or pos[opinion - 1].__contains__('JJR'):
-              o = tok[opinion-1]
-              t = tok[target-1]
-              list_opinion.append(o)
-              #O-->O-dep-->T
+      with open(file_target) as myfile:
+        if sost in myfile.read():
+          MR = ['amod', 'advmod', 'rcmod']
+          for items in depe:
+            if items[0] in MR:
+              opinion = items[2]
+              target = items[1]
+              if pos[opinion - 1].__contains__('JJ') or pos[opinion - 1].__contains__('JJS') \
+                      or pos[opinion - 1].__contains__('JJR'):
+                o = tok[opinion-1]
+                t = tok[target-1]
+                list_opinion.append(o)
+                #O-->O-dep-->T
 
-            for items in depe:
-              if items[0] == 'nsubj':
-                if items[1] == target:
-                  if pos[opinion-1].__contains__('JJ') or pos[opinion-1].__contains__('JJS') \
-                          or pos[opinion-1].__contains__('JJR'):
-                    o = tok[opinion - 1]
-                    list_opinion.append(o)
-                    # O-->O-dep-->H<--T-dep<--T
+              for items in depe:
+                if items[0] == 'nsubj':
+                  if items[1] == target:
+                    if pos[opinion-1].__contains__('JJ') or pos[opinion-1].__contains__('JJS') \
+                            or pos[opinion-1].__contains__('JJR'):
+                      o = tok[opinion - 1]
+                      list_opinion.append(o)
+                      # O-->O-dep-->H<--T-dep<--T
 
   return set(list_opinion)
 
 
+def R3(local_corenlp_path, input, file_target):
 
+  toke, pos, depe = SC(local_corenlp_path, input)
+  list_target = []
 
+  print("Tokeninze: ", toke)
+  print('\n')
+  print("Part Of Speech: ", pos)
+  print('\n')
+  print("Dependency Parse: ", depe)
+  print('\n')
+
+  tok = np.array(toke)
+  #CONJ = ['conj']
+  for item in depe:
+    if item[0] == 'conj':
+      n = tok[item[1]-1]
+      with open(file_target) as myfile:
+        if n in myfile.read():
+          t = item[2]
+          for item in pos:
+            if item[1].__contains__('NN'):
+              target = tok[t-1]
+              list_target.append(target)
+
+  return set(list_target)
 
--- StanfordParser/main.py
+++ StanfordParser/main.py
@@ -1,36 +1,68 @@
 import SCnlp
 import sys
-
+from nltk.tokenize import sent_tokenize
+import nltk
 local_corenlp_path = 'stanford-corenlp-4.0.0/stanford-corenlp-4.0.0'
-
+'''
 with open('input.txt', 'r') as infile, \
         open('input_processato.txt', 'w') as outfile:
   data = infile.read()
   data = data.replace(".", "")
   data = data.replace(",", "")
   outfile.write(data)
-
-
+'''
+'''
 text = open('input_processato.txt', 'r')
 lines = text.readlines()
 input = ''
 for i in lines:
   input = input + i
+'''
+
+nltk.download('punkt')
+text = open("input.txt").read()
+sentences = sent_tokenize(text)
 
 
-Tokenize, POS, Dependency = SCnlp.SC(local_corenlp_path, input)
+#sys.stdout = open("output.txt", "w")
 
-sys.stdout = open("output.txt", "w")
 
-print("REGOLA 1")
-print('\n')
-target = SCnlp.R1(local_corenlp_path, input)
-print("lista dei potenziali target: ", target)
-print('\n')
+target = open('target.txt', 'w')
+for input in sentences:
+    print("REGOLA 1")
+    print('\n')
+    targ = SCnlp.R1(local_corenlp_path, input)
+    for element in targ:
+        target.write(element)
+        target.write('\n')
 
-print("REGOLA 2")
-print('\n')
-opinion = SCnlp.R2(local_corenlp_path, input)
-print("lista delle potenziali opinion words: ", opinion)
+target.close()
 
-sys.stdout.close()
+opinion = open('opinion.txt', 'w')
+target = 'target.txt'
+for input in sentences:
+    print("REGOLA 2")
+    print('\n')
+    opin = SCnlp.R2(local_corenlp_path, input, target)
+    for element in opin:
+        opinion.write(element)
+        opinion.write('\n')
+
+opinion.close()
+
+#sys.stdout.close()
+
+target = open('target.txt', 'a')
+targe = 'target.txt'
+for input in sentences:
+    print("REGOLA 3")
+    print('\n')
+    targ=SCnlp.R3(local_corenlp_path, input, targe)
+    for element in targ:
+        target.write(element)
+        target.write('\n')
+
+target.close()
+
+
+
